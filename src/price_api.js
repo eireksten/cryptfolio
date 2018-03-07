d5fb7d63998d3e0fcdb346679c3d46646576b26b
Eirik Reksten <eirik.reksten.acano.com>
2018-03-07 14:07:01 +0100

    Basic command line interface added.

--- src/price_api.js
+++ src/price_api.js
@@ -2,16 +2,16 @@ import request from 'request';
 
 export function getPricingData() {
 
-    return new Promise(function (resolve, reject) {
-        request('https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=200', function (error, response, body) {
+  return new Promise(function (resolve, reject) {
+    request('https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=200', function (error, response, body) {
 
-            if (error) {
-                reject(error);
-            } else {
-                resolve(JSON.parse(body));
-            }
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
 
-        });
     });
+  });
 
 }
